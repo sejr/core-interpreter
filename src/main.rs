442fed45e519b90927b1b334b80746a8c60a29a0
Samuel Roth <s-roth@onu.edu>
2017-02-03 16:56:26 +0000

    Rudimentary parsers for assignment, equality, or, semicolon.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ enum Token {
     Integer = 31,
     Identifier = 32,
     Semicolon = 12,
-    Equal = 14,
-    Or = 19,
-    And = 26,
+    OpAssignment = 14,
+    OpOr = 19,
+    OpEquality = 26,
     Error = -1,
     EOF = 0,
 }
@@ -17,10 +17,9 @@ mod core {
 
     use Token;
     use std::fs::File;
-    use std::io;
     use std::io::prelude::*;
-    use std::io::{BufRead, BufReader};
-    
+    use std::io::BufReader;
+
     pub fn is_valid_input(arg_count:usize) -> bool {
         /*
          * Takes an unsigned integer as input, which corresponds to the number of arguments
@@ -57,15 +56,50 @@ mod core {
         // A buffer to store the characters we read in from the file (ASCII = 1 byte).
         let mut buf = Vec::<u8>::new();
 
-        // Iterating through the characters of the file . . .
-        
+        // Iterating through the characters of the file . . .        
         f.read_to_end(&mut buf).expect("read_to_end failed");
+        
+        /*
+        // Throwing them all into a string because I don't care about memory
         let s = String::from_utf8(buf).expect("from_utf8 failed");
         for c in s.chars() {
             println!("{}", c);
+        }*/
+
+        let mut skip_flag:bool = false;
+
+        for c in 0 .. buf.len() {
+            // For debugging: println!("Byte: {}", buf[c] as char);
+            if buf[c] == ';' as u8 {
+                println!("Token: {}", Token::Semicolon as u8);
+            }
+            
+            // TODO: Review code to ensure no index-related panics
+            else if buf[c] == '=' as u8 {
+                if buf[c + 1] == '=' as u8 {
+                    if !skip_flag {
+                        println!("Token: {}", Token::OpEquality as u8);
+                        skip_flag = !skip_flag;
+                    }
+                } else {
+                    println!("Token: {}", Token::OpAssignment as u8);
+                }
+            }
+
+            // TODO: Review code to ensure no index-related panics
+            else if buf[c] == '|' as u8 {
+                if c < buf.len() - 1 {
+                    if buf[c + 1] == '|' as u8 {
+                        println!("Token: {}", Token::OpOr as u8);
+                        skip_flag = !skip_flag;
+                    } else {
+                        println!("Token: {}", Token::Error as i8);
+                    }
+                }
+            }
         }
 
-        buf = s.into_bytes();
+        // buf = s.into_bytes();
         buf.clear();
     }
 
