fcbe8b7bc28db66f84853b3594183d6cd5e9c084
Samuel Roth <s-roth@onu.edu>
2017-02-03 10:43:51 +0000

    Parser beginnings, and test for is_valid_input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 enum Token {
     Integer     = 31,
     Identifier  = 32,
@@ -10,10 +12,12 @@ enum Token {
 }
 
 mod core {
-    use std::env;
-    pub fn is_valid_input() -> bool {
-        let args: Vec<String> = env::args().collect();
-        if args.len() != 2 {
+    
+    // use std::env;
+    // use std::fs::File;
+
+    pub fn is_valid_input(arg_count:usize) -> bool {
+        if arg_count != 2 {
             return false;
         } else {
             return true;
@@ -24,19 +28,31 @@ mod core {
         println!("Usage: ./core_interpreter <core-source-file-name>");
     }
 
+    pub fn parse_file(file:&String) {
+        println!("{}", file);
+    }
+
     #[cfg(test)]
     mod test {
         #[test]
-        fn test_eq() {
-            assert_eq!(1, 1);
+        fn test_is_valid_input() {            
+            let case_a: Vec<String> = vec!["Hello ".to_string(), "world!".to_string()];
+            let case_b: Vec<String> = vec!["This ".to_string(), "won't ".to_string(), "work!".to_string()];
+            let case_c: Vec<String> = vec!["Goodbye!".to_string()];
+
+            assert_eq!(super::is_valid_input(case_a.len()), true, "Case A should be valid, but wasn't.");
+            assert_eq!(super::is_valid_input(case_b.len()), false, "Case B should be invalid, but wasn't.");
+            assert_eq!(super::is_valid_input(case_c.len()), true, "Case C should be invalid, but wasn't.");
         }
     }
 }
 
 fn main() {
-    if (!core::is_valid_input()) {
+    let args: Vec<String> = env::args().collect();
+    if !core::is_valid_input(args.len()) {
         core::print_usage();
     } else {
-        println!("Hello!");
+        let ref file:String = args[1];
+        core::parse_file(file);
     }
 }
