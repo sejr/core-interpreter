5e2ebfa8f9abcd9278d8cd356a3b250cee09ee25
Samuel Roth <s-roth@onu.edu>
2017-02-03 13:51:46 +0000

    We can read file contents

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ enum Token {
 }
 
 mod core {
-    
-    // use std::env;
-    // use std::fs::File;
+
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
 
     pub fn is_valid_input(arg_count:usize) -> bool {
         /* 
@@ -44,7 +44,25 @@ mod core {
          * files and reading in characters. Those characters will be used to form the designated
          * tokens, which will then be processed later on.
          */
-        println!("{}", file);
+        println!("Processing input {}", file);
+
+        // Initializing a BufReader based on the file provided as input.
+        let mut f = BufReader::new(File::open(file).expect("File failed to open."));
+
+        // A buffer to store the characters we read in from the file (ASCII = 1 byte).
+        let mut buf = Vec::<u8>::new();
+
+        // Iterating through the characters of the file . . .
+        while f.read_until(b'\n', &mut buf).expect("read_until failed") != 0 {
+
+            // Moving the read data into a string
+            let s = String::from_utf8(buf).expect("from_utf8 failed");
+            for c in s.chars() {
+                println!("Char: {}", c);
+            }
+            buf = s.into_bytes();
+            buf.clear();
+        }
     }
 
     #[cfg(test)]
