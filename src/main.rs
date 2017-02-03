87101805cf8c6b07d3adf6789886c5f8471e0d85
Samuel Roth <s-roth@onu.edu>
2017-02-03 02:45:12 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+enum Token {
+    Integer     = 31,
+    Identifier  = 32,
+    Semicolon   = 12,
+    Equal       = 14,
+    Or          = 19,
+    And         = 26,
+    Error       = -1,
+    EOF         = 0,
+}
+
+mod core_interpreter {
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
