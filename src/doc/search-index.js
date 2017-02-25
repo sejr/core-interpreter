7d57f7c665300236f101cda36096ede4db4aef2c
Samuel Roth <s-roth@onu.edu>
2017-02-25 03:35:53 +0000

    Meh. Docs later

--- src/doc/search-index.js
+++
@@ -1,3 +0,0 @@
-var searchIndex = {};
-searchIndex["tokenizer"] = {"doc":"The tokenizer takes the Core source file and converts it to a vector of parsable Tokens.","items":[[5,"is_valid_input","tokenizer","Verifies that the correct number (2) of arguments were passed.",null,{"inputs":[{"name":"usize"}],"output":{"name":"bool"}}],[5,"print_usage","","Informs the user of how to invoke the interpreter.",null,{"inputs":[],"output":null}],[5,"init_driver","","",null,{"inputs":[{"name":"string"}],"output":null}]],"paths":[]};
-initSearch(searchIndex);
--- src/doc/tokenizer/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["init_driver",""],["is_valid_input","Verifies that the correct number (2) of arguments were passed."],["print_usage","Informs the user of how to invoke the interpreter."]]});
